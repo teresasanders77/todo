dd1068c81761bade552856232f28daa7b9afec2d
Teresa Loafman <teresasanders77@gmail.com>
2020-09-01 11:28:02 -0600

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,123 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <h1>To-Do List</h1>
+    <md-card class="md-primary" md-with-hover>
+      <md-ripple>
+        <md-field>
+          <md-input v-model="currentTodo" @keydown.enter="addTodo()" placeholder="Add a todo"></md-input>
+        </md-field>
+      </md-ripple>
+      <ul class="todos">
+        <md-list v-for="todo in todos" :key="todo.id">
+          <v-row class="row">
+            <input class="checkboxButton" type="checkbox" v-model="todo.completed" />
+            <span
+              class="todo-item-label"
+              :class="{completed: todo.completed}"
+              @dblclick="editTodo(todo)"
+              v-if="!todo.edit"
+            >{{ todo.label }}</span>
+            <md-input
+              v-else
+              class="todo-item-edit"
+              type="text"
+              v-model="todo.label"
+              @blur="doneEdit(todo)"
+              @keyup.enter="doneEdit(todo)"
+              @keyup.escape="doneEdit(todo)"
+            ></md-input>
+            <button @click="removeTodo(todo)" class="x-btn">
+              <x-icon size="1.5x" class="custom-class"></x-icon>
+            </button>
+          </v-row>
+        </md-list>
+      </ul>
+    </md-card>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { XIcon } from "vue-feather-icons";
 
 export default {
-  name: 'App',
+  data() {
+    return {
+      todos: [],
+      currentTodo: "",
+      editedTodo: null,
+    };
+  },
   components: {
-    HelloWorld
-  }
-}
+    XIcon,
+  },
+
+  methods: {
+    addTodo() {
+      this.todos.push({
+        id: this.todos.length,
+        label: this.currentTodo,
+        completed: false,
+        edit: false,
+      });
+      this.currentTodo = "";
+    },
+    removeTodo(todo) {
+      var index = this.todos.indexOf(todo);
+      this.todos.splice(index, 1);
+    },
+    editTodo(todo) {
+      todo.edit = true;
+    },
+    doneEdit(todo) {
+      todo.edit = false;
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+.container {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin: 25px 400px 75px 400px;
+  display: block;
+}
+
+@media only screen and (max-width: 600px) {
+  .container {
+    margin: auto;
+    text-align: center;
+  }
+}
+.completed {
+  text-decoration: line-through;
+  text-decoration-color: red;
+}
+.todo-item-label {
+  font-size: 25px;
+}
+.checkboxButton {
+  margin: 25px;
+}
+.todo-item-label {
+  margin: 25px;
+}
+button {
+  margin: 25px;
+  background: none;
+  color: red;
+}
+button:hover {
+  cursor: pointer;
+  color: #a50909;
+}
+.row {
+  border-style: groove;
+  text-align: left;
+}
+.todos {
+  margin-right: 25px;
+}
+h1 {
+  color: white;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 import Vue from 'vue'
+import VueMaterial from 'vue-material'
 import App from './App.vue'
+import { MdField } from 'vue-material/dist/components';
+import 'vue-material/dist/vue-material.min.css';
+import 'vue-material/dist/theme/black-green-dark.css'
+
+Vue.use(MdField);
+Vue.use(VueMaterial)
+
 
 Vue.config.productionTip = false
 
